b9fdc24ee266c8b7cf5d2c76eac4c12abcf5a419
Migz <gbmombay@up.edu.ph>
2022-04-12 21:40:38 +0800

    + tailwind integration

--- app/.erb/configs/webpack.config.renderer.prod.ts
+++ app/.erb/configs/webpack.config.renderer.prod.ts
@@ -57,13 +57,42 @@ const configuration: webpack.Configuration = {
               importLoaders: 1,
             },
           },
-          'sass-loader',
+          {
+            // style, css, sass loaders...
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins:
+                  [
+                    require('tailwindcss'),
+                    require('autoprefixer'),
+                  ]
+              }
+            },
+          },
         ],
         include: /\.module\.s?(c|a)ss$/,
       },
       {
         test: /\.s?(a|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader, 
+          'css-loader', 
+          'sass-loader',
+          {
+            // style, css, sass loaders...
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins:
+                  [
+                    require('tailwindcss'),
+                    require('autoprefixer'),
+                  ]
+              }
+            },
+          },
+        ],
         exclude: /\.module\.s?(c|a)ss$/,
       },
       // Fonts
---
+++ app/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{ejs,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
