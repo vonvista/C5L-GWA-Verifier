41519b638c4cae5041a83991f5ed7c8e82ae1c10
THE11th <vonatsiv1030@gmail.com>
2022-04-17 23:19:28 +0800

    + init files for grades processing

---
+++ app/src/backend/GradeProcessing.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect, useRef } from "react";
+
+// need muna na malaman kung anong DS/Data Type nakastore yung data
+// baka pwede for the criteria of laude ay hashmap ng bools - vov
+
+// process INC, DRP, and DFG 
+const processINC = () => {
+  return
+}
