e103cf6bb60abef9bf7414fb528fdc9cb340dd96
nicolesianquita <nicolekaysianquita@gmail.com>
2022-04-18 22:40:23 +0800

    - removed changes from new branch on App.jsx and index.ejs

--- app/src/renderer/App.jsx
+++ app/src/renderer/App.jsx
@@ -1,10 +1,40 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import StudentHistoryModal from '../frontend/components/StudentHistoryModal';
+import icon from '../../assets/icon.svg';
+import './App.css';
 
 const Hello = () => {
   return (
     <div>
-      <StudentHistoryModal />
+      <div className="Hello">
+        <img width="200px" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+      <div className="Hello">
+        <a
+          href="https://electron-react-boilerplate.js.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Read our docs
+          </button>
+        </a>
+        <a
+          href="https://github.com/sponsors/electron-react-boilerplate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              🙏
+            </span>
+            Donate
+          </button>
+        </a>
+      </div>
     </div>
   );
 };
