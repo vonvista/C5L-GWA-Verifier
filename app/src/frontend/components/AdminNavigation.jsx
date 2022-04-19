95cf6e6912bde728e38626ff50b15041e1c32a71
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2022-04-20 02:00:04 +0800

    Added documentation

---
+++ app/src/frontend/components/AdminNavigation.jsx
@@ -0,0 +1,150 @@
+import 'tailwindcss/tailwind.css';
+import { useState, useCallback, useRef } from 'react';
+import user from '../../../assets/icons/default-user-icon.jpg';
+
+
+/* Importing to Pages
+<div>
+    <div><Sidebar /></div>
+    <div className="absolute inset-0 flex ml-24 items-center">
+        <span>Page Contents</span>    
+    </div>
+</div>
+*/
+
+
+// Referenced from https://www.30secondsofcode.org/react/s/use-hover
+// Used for hover to be able trigger collapse/expansion of sidebar
+const useHover = () => {
+    const [isHovering, setIsHovering] = useState(true);
+  
+    const handleMouseOver = useCallback(() => setIsHovering(true), []);
+    const handleMouseOut = useCallback(() => setIsHovering(false), []);
+  
+    const nodeRef = useRef();
+  
+    const callbackRef = useCallback(
+      node => {
+        if (nodeRef.current) {
+          nodeRef.current.removeEventListener('mouseover', handleMouseOver);
+          nodeRef.current.removeEventListener('mouseout', handleMouseOut);
+        }
+  
+        nodeRef.current = node;
+  
+        if (nodeRef.current) {
+          nodeRef.current.addEventListener('mouseover', handleMouseOver);
+          nodeRef.current.addEventListener('mouseout', handleMouseOut);
+        }
+      },
+      [handleMouseOver, handleMouseOut]
+    );
+  
+    return [callbackRef, isHovering];
+};
+  
+
+const AdminNav = () => {
+    const [hoverRef, isHovering] = useHover();
+      
+    return (
+        <div 
+            ref={hoverRef}
+            className={` ${
+            isHovering ? "w-1/4 xl:w-1/5" : "w-20"
+            } duration-300 bg-primary-red h-screen text-sidebar-text overflow-hidden sticky top-0`}
+            // onMouseOver={() => setOpen(open)}
+            // onMouseLeave={() => setOpen(open)}
+        >
+            {/* <div className="flex place-content-center p-8"> */}
+                {/* <div className={` ${ isHovering? "visible" : "hidden duration-10000" } flex-shrink-0  `}> */}
+                {/* <div className={` ${ isHovering? "flex place-content-center" : "hidden" } py-8`}> */}
+                <div className="flex px-24 py-8">                
+                <div className={` ${ isHovering? "place-content-center" : "" } flex-shrink-0  `}>
+                    Kalatas: CAS GWA Verifier
+                </div>
+            </div>
+
+            {/* User Account Details */}
+            <div className="flex gap-x-4 items-center text-lg">
+            {/* <div className="flex gap-x-4 items-center text-lg lg:text-base"> */}
+                <img
+                    className={` ${isHovering ? "ml-5 duration-500" : "ml-0 scale-50 duration-500" } rounded-full `}
+                    // className={` ${isHovering ? "ml-5 duration-500" : "mt-16 ml-0 scale-50 duration-500" } rounded-full `}
+                    // className={` ${isHovering ? "ml-5 duration-500" : "mt-16 ml-0 scale-50 duration-500" } rounded-full `}
+                    width="85px"
+                    height="85px"
+                    alt="usericon"
+                    src={user}
+                />
+
+                <div className="pl-3 flex-shrink-0">
+                    <div><strong>Juan Dela Cruz</strong></div>
+                    <div>jvdelacruz</div>
+                    <div>SHAC Committee Member</div>
+                </div>
+            </div>
+            
+            {/* Navigation Pages */}
+            <div className="grid grid-cols-1 cursor-pointer text-xl">
+
+                {/* Admin Dashboard */}
+                <div className={` ${isHovering? "mt-10" : "mt-2" } duration-300 flex gap-x-4 items-center p-2 hover:bg-secondary-red hover:text-highlight`}>
+                    <div>
+                        <svg className={` ${!isHovering && "ml-4"} duration-300 ml-5 fill-current`} xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 36 36" >
+                            <path d="M33 19a1 1 0 0 1-.71-.29L18 4.41 3.71 18.71A1 1 0 0 1 2.3 17.3l15-15a1 1 0 0 1 1.41 0l15 15A1 1 0 0 1 33 19Z" class="clr-i-solid clr-i-solid-path-1"/>
+                            <path d="M18 7.79 6 19.83V32a2 2 0 0 0 2 2h7V24h6v10h7a2 2 0 0 0 2-2V19.76Z" class="clr-i-solid clr-i-solid-path-2"/>
+                            <path fill="none" d="M0 0h36v36H0z"/>
+                        </svg>
+                    </div>
+                    <div className="pl-3 flex-shrink-0 ">
+                        <span>Admin Dashboard</span>
+                    </div>
+                </div>
+
+                {/* User System */}
+                <div className="relative inline-flex mt-3 items-center p-2 hover:bg-secondary-red hover:text-highlight">
+                    <div>
+                        <svg className={` ${!isHovering && "ml-3"} ml-4 fill-current duration-300`} xmlns="http://www.w3.org/2000/svg" width="35" height="30" viewBox="0 0 640 512">
+                            <path d="M319.9 320c57.41 0 103.1-46.56 103.1-104s-46.54-104-103.1-104c-57.41 0-103.1 46.56-103.1 104-.9 57.4 45.7 104 103.1 104zm50 32h-99.8C191.6 352 128 411.7
+                            128 485.3c0 14.8 12.7 26.7 28.4 26.7h327.2c15.7 0 28.4-11.9 28.4-26.7 0-73.6-63.6-133.3-142.1-133.3zM512 160c44.18 0 80-35.82 80-80S556.2 0 512 0c-44.18 0-80 35.82-80
+                            80s35.8 80 80 80zm-328.1 56c0-5.449.982-10.63 1.609-15.91C174.6 194.1 162.6 192 149.9 192H88.08C39.44 192 0 233.8 0 285.3 0 295.6 7.887 304 17.62 304h199.5c-20.42-23.8-33.22-54.3-33.22-88zM128
+                            160c44.18 0 80-35.82 80-80S172.2 0 128 0C83.82 0 48 35.82 48 80s35.82 80 80 80zm423.9 32h-61.84c-12.8 0-24.88 3.037-35.86 8.24.6 5.26 1.6 10.36 1.6 15.76 0 33.71-12.78 64.21-33.16 88h199.7c9.76
+                            0 17.66-8.4 17.66-18.7 0-51.5-39.4-93.3-88.1-93.3z"/>
+                        </svg>
+                    </div>
+                    <div className="pl-7 flex-shrink-0">
+                        <span>User System</span>
+                    </div>
+                </div>
+
+                {/* Curriculum Management */}
+                <div className="inline-flex mt-2 items-center p-2 hover:bg-secondary-red hover:text-highlight">
+                    <div>
+                        <svg className={` ${!isHovering && "ml-4"} ml-5 fill-current duration-300`} xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+                            <path d="M8 5h13v2H8V5Zm-5-.5h3v3H3v-3Zm0 6h3v3H3v-3Zm0 6h3v3H3v-3ZM8 11h13v2H8v-2Zm0 6h13v2H8v-2Z"/>
+                        </svg>
+                    </div>
+                    <div className="pl-7 flex-shrink-0">
+                        <span>Requirements Management</span>
+                    </div>
+
+                </div>
+
+                {/* Logout */}
+                <div className="absolute bottom-8 flex gap-x-4 items-center p-2 hover:text-highlight">
+                    <div>
+                        <svg className="ml-5 fill-current" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+                            <path d="M4 18h2v2h12V4H6v2H4V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3zm2-7h7v2H6v3l-5-4 5-4v3z"/>
+                        </svg>
+                    </div>
+                    <div className="pl-7">
+                        <span>Logout</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AdminNav
