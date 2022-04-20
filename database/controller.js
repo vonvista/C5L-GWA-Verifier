95a5defda17b825b4508eb1d11596e0709ca6f59
Megan Echano <mdechano@up.edu.ph>
2022-04-20 17:33:43 +0800

    update controller.js + request.js

--- database/controller.js
+++ database/controller.js
@@ -177,6 +177,7 @@ exports.gradeAdd = function(req, res, next) {
 // COURSE SCHEMA
 const courseSchema = new Schema({
   CourseName: {type: String, required: true, unique: true},
+  CourseAbbr: {type: String, required: true, unique: true},
   Units: {type: Number, required: true},
   CourseType: {type: String, required: true, enum: ['required', 'non-academic', 'electives']}
 },{autoCreate:true});
@@ -197,53 +198,66 @@ exports.courseAdd = function(req, res, next) {
   
   var newCourse = new Course({
     CourseName: req.body.CourseName,
+    CourseAbbr: req.body.CourseAbbr,
     Units: req.body.Units,
     CourseType: req.body.CourseType
   });
   console.log(newCourse);
 
   newCourse.save(function(err) {
-    if (!err) { res.send(newCourse)}
-    else { res.send('Unable to save user') }
+    if (err) {
+      res.send('Unable to save course')
+    } else {
+      res.send(newCourse)
+    }
   });
 }
 
 // find one course
 exports.courseFindOne = function(req, res, next) {
   Course.findOne((err, course) => {
-    if (!err) {res.send(course)}
+    if (err) {
+      res.send('Unable to find course')
+    } else {
+      res.send(course)
+    }
   });
 }
 
 // find all course
 exports.courseFindAll = function(req, res, next) {
   Course.find((err, courses) => {
-    if (!err) { res.send(courses) }
+    if (err) {
+      res.send('Unable to find all courses')
+    } else {
+      res.send(courses)
+    }
   });
 }
 
 // update one course
 exports.courseUpdateOne = function(req, res, next) {
-  Course.updateOne({CourseName: req.body.CourseName}, {"$set":{
+  Course.updateOne({CourseAbbr: req.body.CourseAbbr}, {"$set":{
     CourseName: req.body.CourseName,
+    CourseAbbr: req.body.CourseAbbr,
     Units: req.body.Units,
     CourseType: req.body.CourseType
   }}, {new : true}, function(err, result){
-    if (!err & Course){
-      res.send(result);
-    } else {
+    if (err) {
       res.send('Unable to update course');
+    } else {
+      res.send(result);
     }
   });
 }
 
 // delete one course
 exports.courseDeleteOne = function(req, res, next) {
-  Course.findOneAndDelete({CourseName: req.body.CourseName}, function(err, Course){
-    if(!err && Course) {
-      res.send('Successfully deleted ' + Course.CourseName);
-    } else {
+  Course.findOneAndDelete({CourseAbbr: req.body.CourseAbbr}, function(err, Course){
+    if(err) {
       res.send('Unable to delete course');
+    } else {
+      res.send('Successfully deleted ' + Course.CourseAbbr);
     }
   });
 }
