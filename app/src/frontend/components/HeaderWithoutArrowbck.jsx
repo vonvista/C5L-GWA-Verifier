542ade9109b62fe8cd3fdc303b0125ec46a76bed
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2022-04-22 10:24:30 +0800

    Update HeaderWithoutArrowbck.jsx
    - Removed sample text for header

--- app/src/frontend/components/HeaderWithoutArrowbck.jsx
+++ app/src/frontend/components/HeaderWithoutArrowbck.jsx
@@ -4,7 +4,7 @@ const HeaderWithoutArrowbck = ({ pageTitle }) => {
     return (
         <header className="flex w-full -mr-20 -z-10 items-center absolute top-0 right-0 bg-secondary-red text-sidebar-text py-3">
             <span className="text-3xl font-bold pl-4 2xl:pl-6">
-                USER DASHBOARD{pageTitle}
+                {pageTitle}
             </span>
         </header>
     );
