51d261138db32a1f7c1711ca14e9999a76d7652e
THE11th <vonatsiv1030@gmail.com>
2022-04-24 21:14:40 +0800

    * update index

--- app/src/renderer/index.jsx
+++ app/src/renderer/index.jsx
@@ -3,7 +3,7 @@ import App from './App';
 import LoginPage from '../frontend/LoginPage';
 // render(<LoginPage />, document.getElementById('root'));
 
-render(<App />, document.getElementById('root'));
+render(<LoginPage />, document.getElementById('root'));
 // calling IPC exposed from preload script
 window.electron.ipcRenderer.once('ipc-example', (arg) => {
   // eslint-disable-next-line no-console
