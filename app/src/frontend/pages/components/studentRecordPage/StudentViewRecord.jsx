f0b5213e680bca3b636cbdf34ecfae5ad6dadbd3
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2022-04-26 22:47:06 +0800

    + added navbar and header to page

--- app/src/frontend/pages/components/studentRecordPage/StudentViewRecord.jsx
+++ app/src/frontend/pages/components/studentRecordPage/StudentViewRecord.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect, useRef } from 'react';
 import ActionsBtn from '../../../components/buttons/Dropdown';
 import SemSelect from '../../../components/inputs/DropdownSelect';
+import Header from '../../../components/HeaderWithArrowbck';
+import UserNav from '../../../components/UserNavigation';
 import { Tab } from '@headlessui/react';
 import 'tailwindcss/tailwind.css';
+import './StudentViewRecord.css';
 import Status from './Status';
 import Notes from './Notes';
 
@@ -10,8 +13,8 @@ import Notes from './Notes';
 
 // styling for student detail header
 const detailStyle = {
-    title: "table-cell text-left text-sm",
-    text: "table-cell text-left text-xl font-bold",
+    title: "table-cell text-left text-sm 2xl:text-xl",
+    text: "table-cell text-left text-xl 2xl:text-2xl font-extrabold",
 }
 
 
@@ -39,90 +42,96 @@ const RecordPage = (props) => {
     let [selectedTab] = useState(tabContents)                       // state controller for selecting tabs
 
     return(
-        <div className="w-full p-6">
-            {/* student details */}
-            <div className="w-[95%] flex px-7 py-5 rounded-lg mx-auto bg-sr-dark-gray shadow-lg box-border">
+        <main>
+            <nav class="sticky z-10"><UserNav /></nav>
+            <div className="relative inset-0 flex ml-8 xl:ml-12 justify-center">
+                
+                <header><Header pageTitle={"Student Record"}/></header>
+                <div className='w-full pt-14 lg:pt-16 xl:pt-20 px-6 flex-column'>
 
-                <div className="table w-4/5">
-                    <div className="table-header-group">
-                        <div className="table-row">
-                            <div className={detailStyle.title}>Student Number</div>
-                            <div className={detailStyle.title}>Name</div>
-                            <div className={detailStyle.title}>Degree Program</div>
-                            <div className={detailStyle.title}>Status</div>
+                    {/* student details */}
+                    <div className="w-[95%] flex px-7 py-5 rounded-lg mx-auto bg-sr-dark-gray shadow-lg box-border">
+
+                        <div className="table w-4/5">
+                            <div className="title-styles">
+                                <div className="table-row">
+                                    <div className={detailStyle.title}>Student Number</div>
+                                    <div className={detailStyle.title}>Name</div>
+                                    <div className={detailStyle.title}>Degree Program</div>
+                                    <div className={detailStyle.title}>Status</div>
+                                </div>
+                            </div>
+
+                            <div className="value-styles">
+                                <div className="table-row">
+                                    <div className={detailStyle.text}>{selectedUser.stud_no}</div>
+                                    <div className={detailStyle.text}>{selectedUser.name}</div>
+                                    <div className={detailStyle.text}>{selectedUser.degree_program}</div>
+                                    <div className={detailStyle.text}>{selectedUser.status}</div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className="w-1/5 flex items-center">
+                            <ActionsBtn />
+                        </div>
+
+
+                    </div>
+
+                    {/* student grades */}
+                    <div className="w-[95%] grid grid-flow-col mx-auto my-5 gap-3">
+
+                        <div className="col-span-3">
+                            {/*{/* dropdown select }
+                            <div className="col-span-1 col-start-6">
+                                <SemSelect
+                                    style="w-72 ml-auto mr-0"
+                                    options={semesters}
+                                    state={[selectedSem, setSelectedSem]}     // so state can be accessed by parent component
+                                    />
+                            </div>*/}
+                            {/* grades table */}
+                            <div className="shadow-lg w-[60vw] col-span-3 rounded-lg mt-3 h-80">
+                                {/* insert grades table component here */}
+                            </div>
+                        </div>
+                                    
+                        {/* tabbed information card */}
+
+                        <div className="col-span-1 max-w-[30vw] shadow-lg rounded-lg">
+                            <Tab.Group>
+                                <Tab.List className="flex rounded-t-md">
+                                    {Object.keys(selectedTab).map((tab) => (
+                                            <Tab key={tab}
+                                                className={({selected}) => (
+                                                    selected ? 
+                                                        'transition-all ease-in delay-200 text-login-green pb-2 pt-4 w-1/3 border-b border-login-green' : 
+                                                        'transition-all ease-in delay-200 text-sr-tab-inactive pb-2 pt-4 w-1/3 border-b border-sr-divider-light hover:text-login-green-hover hover:transition-all hover:ease-in hover:delay-300'
+                                                    )
+                                                }
+                                            >
+                                                {tab}
+                                            </Tab>
+                                        )
+                                    )}
+                                </Tab.List>
+                                <Tab.Panels className="w-[30vw] m-0 block">
+                                        {Object.values(selectedTab).map((component) =>(
+                                            <Tab.Panel className="col-span-1 block">                                
+                                                {component}
+                                            </Tab.Panel>
+                                        ))}
+
+                                </Tab.Panels>
+                            </Tab.Group>
+
                         </div>
                     </div>
-
-                    <div className="table-row-group">
-                        <div className="table-row">
-                            <div className={detailStyle.text}>{selectedUser.stud_no}</div>
-                            <div className={detailStyle.text}>{selectedUser.name}</div>
-                            <div className={detailStyle.text}>{selectedUser.degree_program}</div>
-                            <div className={detailStyle.text}>{selectedUser.status}</div>
-                        </div>
-
-                    </div>
                 </div>
-
-                <div className="w-1/5 flex items-center">
-                    <ActionsBtn />
-                </div>
-                 
-
             </div>
-
-            {/* student grades */}
-            <div className="w-[95%] grid grid-flow-col mx-auto my-5 gap-3">
-
-                <div className="col-span-3">
-                    {/*{/* dropdown select }
-                    <div className="col-span-1 col-start-6">
-                        <SemSelect
-                            style="w-72 ml-auto mr-0"
-                            options={semesters}
-                            state={[selectedSem, setSelectedSem]}     // so state can be accessed by parent component
-                            />
-                    </div>*/}
-                    {/* grades table */}
-                    <div className="shadow-lg w-[60vw] col-span-3 rounded-lg mt-3 h-80">
-                        {/* insert grades table component here */}
-                    </div>
-                </div>
-                                
-                {/* tabbed information card */}
-                
-                <div className="col-span-1 max-w-[30vw] shadow-lg rounded-lg">
-                    <Tab.Group>
-                        <Tab.List className="flex rounded-t-md">
-                            {Object.keys(selectedTab).map((tab) => (
-                                    <Tab key={tab}
-                                        className={({selected}) => (
-                                            selected ? 
-                                                'transition-all ease-in delay-200 text-login-green pb-2 pt-4 w-1/3 border-b border-login-green' : 
-                                                'transition-all ease-in delay-200 text-sr-tab-inactive pb-2 pt-4 w-1/3 border-b border-sr-divider-light hover:text-login-green-hover hover:transition-all hover:ease-in hover:delay-300'
-                                            )
-                                        }
-                                    >
-                                        {tab}
-                                    </Tab>
-                                )
-                            )}
-                        </Tab.List>
-                        <Tab.Panels className="w-[30vw] m-0 block">
-                                {Object.values(selectedTab).map((component) =>(
-                                    <Tab.Panel className="col-span-1 block">                                
-                                        {component}
-                                    </Tab.Panel>
-                                ))}
-
-                        </Tab.Panels>
-                    </Tab.Group>
-
-                </div>
-                
-            </div>
-            
-        </div>
+        </main>        
     );
 }
 
