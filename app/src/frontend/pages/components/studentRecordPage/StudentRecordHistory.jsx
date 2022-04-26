c5b9a816a4de480e67909d86c1ec413cf05a3868
Migz <gbmombay@up.edu.ph>
2022-04-26 23:19:58 +0800

    * added props for easier backend management

---
+++ app/src/frontend/pages/components/studentRecordPage/StudentRecordHistory.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable import/named */
+// StudentRecordHistory contains the tab component of the student record history.
+// ----- new changes: modal to tab
+
+// import { useState } from 'react';
+import RecordHistory from './StudentHistory';
+import './css/StudentRecordHistory.css';
+import 'tailwindcss/tailwind.css';
+
+const StudentRecordHistory = ({historyData, ...rest}) => {
+
+  if (historyData.length === 0) {
+    return (
+      <>
+        <div className="p-5 h-[70vh] h-max-[70vh] modal-content">
+          <div className="modal-body">
+            <h2 className="inter font-light text-[1.05vw] italic">
+              No Student Record History found.
+            </h2>
+          </div>
+        </div>
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="p-5 h-[70vh] h-max-[70vh] modal-content overflow-auto">
+        {/* history body; Last modified and descriptions */}
+        <div className="modal-body">
+          {historyData.map(({ date, info, i }) => (
+            <section key={i} className="mb-3">
+              {/* History per date */}
+              <div className="rounded-lg border border-solid border-t-zinc-200 border-b-zinc-200">
+                <h2 className="inter pt-2 pb-2 pl-4 font-light text-sm">
+                  Last Modified: {date}
+                </h2>
+                {/* descriptions of the same date */}
+                {info.map(({ main, user, time, details, j }) => (
+                  <RecordHistory
+                    key={j}
+                    main={main}
+                    user={user}
+                    time={time}
+                    details={details}
+                  />
+                ))}
+              </div>
+            </section>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default StudentRecordHistory;
