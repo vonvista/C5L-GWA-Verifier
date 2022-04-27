42d0adeea48ea3159c6b60fba1ac921577545429
Migz <gbmombay@up.edu.ph>
2022-04-27 22:23:09 +0800

    + dynamic generation of elements

--- app/src/frontend/pages/components/studentRecordPage/TableDivider.jsx
+++ app/src/frontend/pages/components/studentRecordPage/TableDivider.jsx
@@ -5,144 +5,15 @@ import 'tailwindcss/tailwind.css';
 
 import TableContents from './TableContents';
 
-const TableDivider = ({  }) => {
-    const [rows, setRows] = useState([]);
+const TableDivider = ({ grades }) => {
 
-    const FirstSem = [
-        {
-            "courseName": "CMSC 12",
-            "units": "3.0",
-            "grade": "1.00",
-            "enrolled": "3.00",
-            "runningSum": "3.00"
-        },
-        {
-            "courseName": "CMSC 56",
-            "units": "3.0",
-            "grade": "1.50",
-            "enrolled": "4.50",
-            "runningSum": "7.50"
-        },
-        {
-            "courseName": "MATH 27",
-            "units": "3.0",
-            "grade": "1.75",
-            "enrolled": "5.25",
-            "runningSum": "12.75"
-        },
-        {
-            "courseName": "ETHICS 1",
-            "units": "3.0",
-            "grade": "1.50",
-            "enrolled": "4.50",
-            "runningSum": "17.25"
-        },
-        {
-            "courseName": "STS 1",
-            "units": "3.0",
-            "grade": "1.75",
-            "enrolled": "5.25",
-            "runningSum": "22.50"
-        },
-        {
-            "courseName": "HK 11",
-            "units": "2.0",
-            "grade": "1.00",
-            "enrolled": "2.00",
-            "runningSum": "22.50"
-        },
-    ];
-
-    const SecondSem = [
-        {
-            "courseName": "CMSC 21",
-            "units": "3.0",
-            "grade": "1.00",
-            "enrolled": "3.00",
-            "runningSum": "25.50"
-        },
-        {
-            "courseName": "CMSC 57",
-            "units": "3.0",
-            "grade": "1.50",
-            "enrolled": "4.50",
-            "runningSum": "30.00"
-        },
-        {
-            "courseName": "MATH 28",
-            "units": "3.0",
-            "grade": "1.50",
-            "enrolled": "4.50",
-            "runningSum": "34.50"
-        },
-        {
-            "courseName": "KAS 1",
-            "units": "3.0",
-            "grade": "1.75",
-            "enrolled": "5.25",
-            "runningSum": "39.75"
-        },
-        {
-            "courseName": "ARTS 1",
-            "units": "3.0",
-            "grade": "1.00",
-            "enrolled": "3.00",
-            "runningSum": "42.75"
-        },
-    ];
-
-    const ThirdSem = [
-        {
-            "courseName": "CMSC 22",
-            "units": "3.0",
-            "grade": "1.00",
-            "enrolled": "3.00",
-            "runningSum": "45.75"
-        },
-        {
-            "courseName": "CMSC 123",
-            "units": "3.0",
-            "grade": "2.25",
-            "enrolled": "6.75",
-            "runningSum": "52.50"
-        },
-        {
-            "courseName": "CMSC 130",
-            "units": "3.0",
-            "grade": "1.00",
-            "enrolled": "3.00",
-            "runningSum": "55.50"
-        },
-        {
-            "courseName": "CMSC 150",
-            "units": "3.0",
-            "grade": "1.50",
-            "enrolled": "4.50",
-            "runningSum": "60.00"
-        },
-        {
-            "courseName": "SCIENCE 11",
-            "units": "3.0",
-            "grade": "1.50",
-            "enrolled": "4.50",
-            "runningSum": "64.50"
-        },
-        {
-            "courseName": "NSTP 1",
-            "units": "2.0",
-            "grade": "1.25",
-            "enrolled": "2.50",
-            "runningSum": "64.50"
-        },
-    ];
-
-    const Semesters = [FirstSem, SecondSem, ThirdSem];
-    
     return (
-        <div className="w-full p-2 mx-auto bg-white rounded-2xl">
-            <TableContents Name="First Sem A.Y.2019-2020" Semester={Semesters[0]} />
-            <TableContents Name="Second Sem A.Y.2019-2020" Semester={Semesters[1]} />
-            <TableContents Name="First Sem A.Y.2020-2021" Semester={Semesters[2]} />
+        <div className="w-full mx-auto bg-white">
+            {
+                grades.map((semData, idx)=>(
+                    <TableContents key={idx} Name={semData.sem} Semester={semData.data} />
+                ))
+            }
         </div>
     )
 }
