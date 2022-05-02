51aa62a2419a08199773e5be3599becdb51b7002
Migz <gbmombay@up.edu.ph>
2022-05-02 14:56:20 +0800

    * added focus transitions

--- app/src/frontend/components/EditRow.jsx
+++ app/src/frontend/components/EditRow.jsx
@@ -4,7 +4,9 @@ import 'tailwindcss/tailwind.css'
 
 export default EditRow = ({data, changeHandler, onSubmit, toggleHandler, touched, errors, valid}) => {
 
-    const inputStyle = `block box-border focus:outline-none border-b border-sr-disabled-green focus:transition focus:ease-in-out focus:delay-100 focus:border-b focus:border-login-green`
+    //
+
+    const inputStyle = `block box-border focus:outline-none border-b border-sr-disabled-green transition ease-out delay-100 focus:transition focus:ease-in-out focus:delay-100 focus:border-b focus:border-login-green`
     const errorStyle= `block text-center text-sm inter text-secondary-red`
     const centerInput= `w-1/3 text-center mx-auto`
 
@@ -12,14 +14,14 @@ export default EditRow = ({data, changeHandler, onSubmit, toggleHandler, touched
         <form className="table-row inter" onSubmit={onSubmit}>
             <div className="table-cell max-w-1/6">
                 <Input
-                    labelStyle="hidden"                     // styling for label
-                    labelVal="course"                       // label text
-                    inputStyle={`w-2/3 ${inputStyle}`}           // styling for input
-                    name="courseName"                       // name of label-input components
-                    inputType="text"                        // type of input password, email, text, etc.
-                    inputPlaceholder={data.courseName}      // placeholder text for input
-                    value={data.courseName}                 // value of the input
-                    changeHandler={changeHandler}           // change handling
+                    labelStyle="hidden"                                 // styling for label
+                    labelVal="course"                                   // label text
+                    inputStyle={`w-2/3 ${inputStyle}`}                  // styling for input
+                    name="courseName"                                   // name of label-input components
+                    inputType="text"                                    // type of input password, email, text, etc.
+                    inputPlaceholder={data.courseName}                  // placeholder text for input
+                    value={data.courseName}                             // value of the input
+                    changeHandler={changeHandler}                       // change handling
                     //required={true}   
                     />
                 {touched.courseName && errors.courseName && 
@@ -28,62 +30,62 @@ export default EditRow = ({data, changeHandler, onSubmit, toggleHandler, touched
             </div>
             <div className="table-cell w-1/6">
                 <Input
-                    labelStyle="hidden"       // styling for label
-                    labelVal=""         // label text
-                    inputStyle={`${centerInput} ${inputStyle}`}       // styling for input
-                    name="units"             // name of label-input components
-                    inputType="numeric"        // type of input password, email, text, etc.
-                    inputPlaceholder={data.units} // placeholder text for input
-                    value={data.units}            // value of the input
-                    changeHandler={changeHandler}    // change handling
+                    labelStyle="hidden"                                 // styling for label
+                    labelVal=""                                         // label text
+                    inputStyle={`${centerInput} ${inputStyle}`}         // styling for input
+                    name="units"                                        // name of label-input components
+                    inputType="numeric"                                 // type of input password, email, text, etc.
+                    inputPlaceholder={data.units}                       // placeholder text for input
+                    value={data.units}                                  // value of the input
+                    changeHandler={changeHandler}                       // change handling
                     //required={true}    
                     />
                 {touched.units && errors.units && 
-                    <p className={errorStyle}>{errors.units}</p>    // errror message
+                    <p className={errorStyle}>{errors.units}</p>        // errror message
                 }   
             </div>
             <div className="table-cell w-1/6">
                 <Input
-                    labelStyle="hidden"       // styling for label
-                    labelVal=""         // label text
-                    inputStyle={`${centerInput} ${inputStyle}`}       // styling for input
-                    name="grade"             // name of label-input components
-                    inputType="numeric"        // type of input password, email, text, etc.
-                    inputPlaceholder={data.grade} // placeholder text for input
-                    value={data.grade}            // value of the input
-                    changeHandler={changeHandler}    // change handling
+                    labelStyle="hidden"                                 // styling for label
+                    labelVal=""                                         // label text
+                    inputStyle={`${centerInput} ${inputStyle}`}         // styling for input
+                    name="grade"                                        // name of label-input components
+                    inputType="numeric"                                 // type of input password, email, text, etc.
+                    inputPlaceholder={data.grade}                       // placeholder text for input
+                    value={data.grade}                                  // value of the input
+                    changeHandler={changeHandler}                       // change handling
                     //required={true}    
                     />
                 {touched.grade && errors.grade && 
-                    <p className={errorStyle}>{errors.grade}</p>    // errror message
+                    <p className={errorStyle}>{errors.grade}</p>        // errror message
                 }   
             </div>
             <div className="table-cell w-1/6">
                 <Input
-                    labelStyle="hidden"       // styling for label
-                    labelVal=""         // label text
-                    inputStyle={`${centerInput} ${inputStyle}`}       // styling for input
-                    name="enrolled"             // name of label-input components
-                    inputType="numeric"        // type of input password, email, text, etc.
-                    inputPlaceholder={data.enrolled} // placeholder text for input
-                    value={data.enrolled}            // value of the input
-                    changeHandler={changeHandler}    // change handling
+                    labelStyle="hidden"                                 // styling for label
+                    labelVal=""                                         // label text
+                    inputStyle={`${centerInput} ${inputStyle}`}         // styling for input
+                    name="enrolled"                                     // name of label-input components
+                    inputType="numeric"                                 // type of input password, email, text, etc.
+                    inputPlaceholder={data.enrolled}                    // placeholder text for input
+                    value={data.enrolled}                               // value of the input
+                    changeHandler={changeHandler}                       // change handling
                     //required={true}    
                     />
                 {touched.enrolled && errors.enrolled && 
-                    <p className={errorStyle}>{errors.enrolled}</p>    // errror message
+                    <p className={errorStyle}>{errors.enrolled}</p>     // errror message
                 }   
             </div>
             <div className="table-cell w-1/6">
                 <Input
-                    labelStyle="hidden"       // styling for label
-                    labelVal=""         // label text
-                    inputStyle={`${centerInput} ${inputStyle}`}       // styling for input
-                    name="runningSum"             // name of label-input components
-                    inputType="numeric"        // type of input password, email, text, etc.
-                    inputPlaceholder={data.runningSum} // placeholder text for input
-                    value={data.runningSum}            // value of the input
-                    changeHandler={changeHandler}    // change handling
+                    labelStyle="hidden"                                 // styling for label
+                    labelVal=""                                         // label text
+                    inputStyle={`${centerInput} ${inputStyle}`}         // styling for input
+                    name="runningSum"                                   // name of label-input components
+                    inputType="numeric"                                 // type of input password, email, text, etc.
+                    inputPlaceholder={data.runningSum}                  // placeholder text for input
+                    value={data.runningSum}                             // value of the input
+                    changeHandler={changeHandler}                       // change handling
                     //required={true}    
                     />
                 {touched.runningSum && errors.runningSum && 
