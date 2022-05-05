45f0eaa4aa74c940473ad365b95c2efc7fdf7424
THE11th <vonatsiv1030@gmail.com>
2022-05-05 22:47:32 +0800

    + get ip

--- database/netin.js
+++ database/netin.js
@@ -5,5 +5,12 @@ const options = {
   ipVersion: 4      // integer (4 or 6): only acknowledge addresses of this IP address family (undefined: both)
 };
 
+
+
 const interfaceNames = ni.getInterfaces(options);
 console.log(interfaceNames)
+
+for (let interfaceName of interfaceNames) {
+  const addresses = ni.toIp(interfaceName, options);
+  console.log( `${interfaceName}: ${addresses}`);
+}
