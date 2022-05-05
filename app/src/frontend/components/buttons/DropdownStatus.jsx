7472302103b12e4b7b9b795d6b7bbcfc3f34bf13
Eyds-Angeles <cbangeles@up.edu.ph>
2022-05-05 16:47:15 +0800

    ~ revised dropdownstatus button appearance
    - revised green accents to white

--- app/src/frontend/components/buttons/DropdownStatus.jsx
+++ app/src/frontend/components/buttons/DropdownStatus.jsx
@@ -1,6 +1,6 @@
 import 'tailwindcss/tailwind.css';
 import { useState } from 'react';
-import expand from '../../../../assets/icons/collapse(1).svg';
+import expand from '../../../../assets/icons/collapse(2).svg';
 
 // Function contains the buttons in Actions Dropdown seen in Student Record View/Edit Page
 // Additional references: https://tailwindui.com/components/application-ui/elements/dropdowns
@@ -13,7 +13,7 @@ const Dropdown = () => {
       <div className="grid-cols-2 divide-x w-40 p-2 bg-white mr-0 rounded-lg flex inline-flex items-center justify-items-center rounded-lg">
         <button
           type="button"
-          className="pl-1.75 m-0 inline-block grow hover:bg-login-green-hover rounded-l-lg"
+          className="pl-1.75 m-0 inline-block grow hover:bg-yellow-100  rounded-l-lg"
           // add onclick
         >
           <p className="pl-1.5 m-0 inline-block grow text-center ">
@@ -22,14 +22,14 @@ const Dropdown = () => {
         </button>
         <button
           type="button"
-          className="pl-1.5 m-0 inline-block bg-login-green grow hover:bg-login-green-hover rounded-r-lg"
+          className="pl-1.5 m-0 inline-block grow hover:bg-yellow-100  rounded-r-lg"
           onClick={() => {
             setIsActive(!isActive);
           }}
         >
           <section className="pl-1.5 m-0 inline-block grow">
             <img
-              className="p-0.25 inline-flex"
+              className="p-0.25 inline-flex "
               width="20px"
               height="20px"
               alt="icon"
@@ -44,36 +44,36 @@ const Dropdown = () => {
           className="origin-top-right z-50 absolute right-0 mt-0.5 w-full rounded-md shadow-lg ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
           tabIndex="-1"
         >
-          <div className="pt-1 bg-login-green rounded-lg">
+          <div className="pt-1 bg-white rounded-lg">
             <button
-              className="text-gray-700 block px-4 py-2 text-sm z-1 w-full hover:bg-login-green-hover hover:rounded-t-lg"
+              className="text-gray-700 block px-4 py-2 text-sm z-1 w-full hover:bg-yellow-100  hover:rounded-t-lg"
               type="button"
               onClick={() => {
                 setIsActive(!isActive);
                 setValueClicked('Unchecked');
               }}
             >
-              <p className="text-white">Unchecked</p>
+              <p>Unchecked</p>
             </button>
             <button
-              className="text-gray-700 block px-4 py-2 text-sm z-1 w-full hover:bg-login-green-hover hover:rounded-t-lg"
+              className="text-gray-700 block px-4 py-2 text-sm z-1 w-full hover:bg-yellow-100  hover:rounded-t-lg"
               type="button"
               onClick={() => {
                 setIsActive(!isActive);
                 setValueClicked('Pending');
               }}
             >
-              <p className="text-white">Pending</p>
+              <p>Pending</p>
             </button>
             <button
-              className="text-gray-700 block px-4 py-2 text-sm z-1 w-full hover:bg-login-green-hover hover:rounded-t-lg"
+              className="text-gray-700 block px-4 py-2 text-sm z-1 w-full hover:bg-yellow-100  hover:rounded"
               type="button"
               onClick={() => {
                 setIsActive(!isActive);
                 setValueClicked('Checked');
               }}
             >
-              <p className="text-white">Checked</p>
+              <p>Checked</p>
             </button>
           </div>
         </div>
