367179018877437b0ce71d7c94304c9354698264
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2022-05-07 17:50:32 +0800

    * updated import path for Notes

--- app/src/frontend/pages/student-record/StudentViewRecord.jsx
+++ app/src/frontend/pages/student-record/StudentViewRecord.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect, useRef } from 'react';
 import ActionsBtn from 'frontend/components/buttons/Dropdown';
 import SemSelect from 'frontend/components/inputs/DropdownSelect';
 import Status from './tabbed-components/status/Status';
-import Notes from '../components/studentRecordPage/Notes';
+import Notes from './tabbed-components/notes/Notes';
 import History from '../components/studentRecordPage/StudentRecordHistory';
 import CheckList from './tabbed-components/checklist/ChecklistTab';
 import Table from './grades-table/TableContents';
