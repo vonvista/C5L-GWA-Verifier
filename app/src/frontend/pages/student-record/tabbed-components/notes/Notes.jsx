d5861a5722c0273a372865a2fad5b08de4324189
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2022-05-14 17:57:01 +0800

    * modified code to lift state up to parent component properly

--- app/src/frontend/pages/student-record/tabbed-components/notes/Notes.jsx
+++ app/src/frontend/pages/student-record/tabbed-components/notes/Notes.jsx
@@ -3,50 +3,51 @@ import AddNote from './AddNote';
 import Delete from '../../../../../../assets/icons/delete.svg';
 
 
-// functional component for notes tab in student record view page
-// -- props will be for placing values into the page
+// Functional component for notes tab in student record view page
+// -- notesData prop : gets the current state of array of notes from parent component
+// -- semesters      : gets the list of semesters that the student has enrolled in; to be used for dropdown select
+// -- setNotesData:  : used to update the state for the notesData in parent component
 
-export default function NotesTab({notes, semesters, notesHandler}) { 
-    const [notesList, setNotesList] = useState(notes)
-    const [selectedSem, setSelectedSem] = useState(semesters[0])    // state controller for selecting sem notes 
-                                                                    // uses first object as default
-       
+export default function NotesTab({notesData, semesters, setNotesData}) { 
+
+    // State that handles selection of sem notes; uses first object as default
+    const [selectedSem, setSelectedSem] = useState(semesters[0])
+    
+
     // Handler for adding/editing notes
     const handleAddNote = (text) => {
-        const newNote = {
+        let currNotesList = [...notesData]      // make copy of current array of notes
+
+        const newNote = {                       // object for new note to be added
             sem: selectedSem.sem,
             Semyear: selectedSem.sem,
             content: text,
         }
         
         // check if note for the sem already exists
-        const index = notesList.map(i => i.sem).indexOf(selectedSem.sem);
-        let newNotesList = [...notesList]
+        const index = notesData.map(i => i.sem).indexOf(selectedSem.sem);
 
         if (index != -1){
-            newNotesList[index] = newNote               // replace note from notesList if it already exists
+            currNotesList[index] = newNote              // replace note from currNotesList if it already exists
         } else {
-            newNotesList = [...notesList, newNote]      // add new note at the end of notesList
+            currNotesList = [...notesData, newNote]     // add new note at the end of currNotesList
         }
         
-        notesHandler(newNote)
-        setNotesList(newNotesList)
+        setNotesData(currNotesList)
 	}
 
     // Handler for deleting notes
     const handleDeleteNote = (values) => {
-        // Get array index of object that will be deleted
-        const targetIndex = notesList.findIndex(obj => obj.sem == values.sem)
+        // get array index of object that will be deleted
+        const targetIndex = notesData.findIndex(obj => obj.sem == values.sem)
         
-        let newNotes = [...notesList]
+        let newNotes = [...notesData]
         newNotes.splice(targetIndex, 1)
 
         deleteNote = {
             Student: localStorage.getItem("currStudentID"),
             Semyear: values.Semyear,
         }
-
-        // console.log(deleteNote)
 
         // fetch post request to delete a note on button click
         fetch(`http://localhost:3001/note/delete`, {
@@ -64,44 +65,50 @@ export default function NotesTab({notes, semesters, notesHandler}) {
 
             })
 
-        setNotesList(newNotes)
+        setNotesData(newNotes)
     }
 
+
     return(
+
         <div className="min-w-[25vw] max-w-[25vw] h-[41rem] max-h-[41rem] mx-auto p-5 block overflow-auto">
             {/* Add note button */}
             <AddNote
-                notesList={notesList}
+                notesList={notesData}
                 semesters={semesters}
                 handleAddNote={handleAddNote}
-                selectedSem={selectedSem}
                 setSelectedSem={setSelectedSem}
+                selectedSem={selectedSem}
             />
 
-            {/* Notes */}
-            { notesList.length === 0
-            ? <h2 className="inter font-light text-[1.05vw] italic">
-                No notes found.
-              </h2>
-            : notesList.map( (data, idx) => {
-                return(
-                    <div className="grid border rounded-lg p-5 mb-2" key={idx}>
-                        <h1 className="text-xl inter font-bold">
-                            {data.Semyear}
-                        </h1>
-                        <p className="inter text-sm mt-3">
-                            {data.content}
-                            {data.Details}
-                        </p>
-                        <button className='w-max rounded-lg mt-4 p-2 flex content-center hover:bg-gray-300' onClick={() => handleDeleteNote(data)}>
-                            <img
-                                width="18"
-                                src={Delete}
-                            />
-                            <span className="text-sm ml-2">Delete</span>
-                        </button>
-                    </div>
-                )
+            {/* Notes tab */}
+            {/* Checks first if the array of notes is currently empty. */}
+            { notesData.length === 0
+                ? <h2 className="inter font-light text-[1.05vw] italic">
+                    No notes found.
+                </h2>
+                : notesData.map( (data, idx) => {
+                    return (
+                        <div className="grid border rounded-lg p-5 mb-2" key={idx}>
+                            {/* Note details */}
+                            <h1 className="text-xl inter font-bold">
+                                {data.Semyear}
+                            </h1>
+                            <p className="inter text-sm mt-3">
+                                {data.content}
+                                {data.Details}
+                            </p>
+
+                            {/* Delete button */}
+                            <button className='w-max rounded-lg mt-4 p-2 flex content-center hover:bg-gray-300' onClick={() => handleDeleteNote(data)}>
+                                <img
+                                    width="18"
+                                    src={Delete}
+                                />
+                                <span className="text-sm ml-2">Delete</span>
+                            </button>
+                        </div>
+                    )
             })}
         </div>
     );
