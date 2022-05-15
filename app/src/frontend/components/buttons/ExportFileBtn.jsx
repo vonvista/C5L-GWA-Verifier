d2014ffd866307bfa2a2a793e3960b135925ab88
Maurice Paguagan <ecpaguagan@up.edu.ph>
2022-05-15 12:33:07 +0800

    - deleted some unnecessary lines of code

--- app/src/frontend/components/buttons/ExportFileBtn.jsx
+++ app/src/frontend/components/buttons/ExportFileBtn.jsx
@@ -2,22 +2,12 @@ import 'tailwindcss/tailwind.css';
 import exportIcon from '../../../../assets/icons/export.svg';
 
 //Button for export file 
-//
-//PROPS:
-//handleClick: function to handle click event
-//
 const ExportFileBtn = () => {
   const exportbtn = `w-[11vw] h-[3vw] bg-export-yellow m-2 rounded-xl text-white text-base font-montserrat font-bold hover:shadow-lg hover:bg-export-yellow-hover`;
 
-  // function that "clicks" the input tag to open file dialog window
-  function exportFile() {
-    // insert backend stuff
-    ;
-  }
-
   return (
     <>
-      <button className={exportbtn} type="button" onClick={exportFile}>
+      <button className={exportbtn} type="button">
         <img
           className="p-0.25 ml-0.25 mr-1.5 inline-flex w-[2vw] h-[2vw]"
           alt="icon"
