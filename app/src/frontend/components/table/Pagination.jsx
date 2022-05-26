ea2d901495b0c7ec3ab9c9b863caea32b3e05f16
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2022-05-27 00:52:38 +0800

    * changed attribute name clip-rule to clipRule and fill-rule to fillRule

--- app/src/frontend/components/table/Pagination.jsx
+++ app/src/frontend/components/table/Pagination.jsx
@@ -93,7 +93,7 @@ const Pagination = ({ rowsPerPage, totalRows, currentPage, paginate }) => {
           {/* Back arrow */}
          <button onClick={prev} className={buttonStyle}>
             <svg className={arrowStyle} viewBox="0 0 20 20">
-               <path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" fill-rule="evenodd"></path>
+               <path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" fillRule="evenodd"></path>
             </svg>
          </button>
          {/* Page Numbers */}
@@ -105,7 +105,7 @@ const Pagination = ({ rowsPerPage, totalRows, currentPage, paginate }) => {
          {/* Next arrow */}
          <button onClick={next} className={buttonStyle}>
             <svg className={arrowStyle} viewBox="0 0 20 20">
-               <path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" fill-rule="evenodd"></path>
+               <path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" fillRule="evenodd"></path>
             </svg>
          </button>
       </div>
