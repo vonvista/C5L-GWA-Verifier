f0d27485c4d46b7d59e9a3a5c485dc6343c3ca31
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2022-05-27 00:52:00 +0800

    + added login button disabled attribute

--- app/src/frontend/components/buttons/LoginBtn.jsx
+++ app/src/frontend/components/buttons/LoginBtn.jsx
@@ -7,12 +7,13 @@ import 'tailwindcss/tailwind.css';
 /* Props:
     handleClick  ---  function to handle click event
 */
-export const LoginBtn = ({ handleClick }) => {
-  const loginbtn = `w-6/12 h-11 1.75xl:text-lg bg-button-green rounded-lg text-white font-montserrat font-semibold hover:shadow-lg hover:bg-button-green-hover`;
+export const LoginBtn = ({ handleClick, disabled }) => {
+  const loginbtn = `w-6/12 h-11 1.75xl:text-lg bg-button-green rounded-lg text-white font-montserrat font-semibold hover:shadow-lg hover:bg-button-green-hover
+                    disabled:bg-sr-disabled-green disabled:hover:bg-sr-disabled-green`;
 
   return (
     <>
-      <button className={loginbtn} type="submit" onClick={handleClick}>
+      <button className={loginbtn} type="submit" onClick={handleClick} disabled={disabled}>
         Log In
       </button>
     </>
